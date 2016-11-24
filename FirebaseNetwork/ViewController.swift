c20dc6cb26a2469d003b463b0580b0d9b13304f8
Lucheo Antonio Tombini Filho <lucheo@Lucheos-MacBook-Pro.local>
2016-11-24 20:58:11 -0200

    Initial Commit

---
+++ FirebaseNetwork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseNetwork
+//
+//  Created by Lucheo Antonio Tombini Filho on 24/11/16.
+//  Copyright © 2016 Lucheo Antonio Tombini Filho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
