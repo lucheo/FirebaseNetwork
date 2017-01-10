221989e30b7fa29ebba0795f0bf8cb0c2b8025e4
Lucheo Antonio Tombini Filho <lucheo@Lucheos-MacBook-Pro.local>
2017-01-10 12:13:21 -0200

    Firebase get done

--- FirebaseNetwork/FeedVC.swift
+++ FirebaseNetwork/FeedVC.swift
@@ -13,6 +13,8 @@ import SwiftKeychainWrapper
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var mainTable: UITableView!
+    
+    var posts = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +23,19 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         mainTable.dataSource = self
         
         DataService.ds.Ref_Posts.observe(.value, with: { (snapshot) in
-            print(snapshot.value)
+            if let snapshots = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for snap in snapshots {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.mainTable.reloadData()
         })
+        
 
 
     }
@@ -33,7 +46,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
         
     }
     
