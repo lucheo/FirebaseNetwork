2fcae95bfe50028701488d971b560bc270de4d30
Lucheo Antonio Tombini Filho <lucheo@Lucheos-MacBook-Pro.local>
2017-01-11 08:35:10 -0200

    Completed Firebase display

--- FirebaseNetwork/PostCell.swift
+++ FirebaseNetwork/PostCell.swift
@@ -15,16 +15,21 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var postImg: UIImageView!
     @IBOutlet weak var caption: UITextView!
     @IBOutlet weak var likesLbl: UILabel!
+    
+    var post: Post!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    func configureCell(post: Post) {
+        self.post = post
+        self.caption.text = post.caption
+        self.likesLbl.text = "\(post.likes)"
+        
+        
     }
+
 
 }
