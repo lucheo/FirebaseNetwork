edfdfb8e90437aa90a1de4db15b199f9aac4ca00
Lucheo Antonio Tombini Filho <lucheo@Lucheos-MacBook-Pro.local>
2017-01-11 15:10:01 -0200

    Finished uploadind to firebase

--- FirebaseNetwork/FeedVC.swift
+++ FirebaseNetwork/FeedVC.swift
@@ -15,8 +15,11 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
     @IBOutlet weak var mainTable: UITableView!
     @IBOutlet weak var addImage: CircleImgView!
     
+    @IBOutlet weak var captionField: MaterialTextField!
+    
     var posts = [Post]()
     var imagePicker: UIImagePickerController!
+    var imageSelected: Bool = false
     
     //static let imageCache: Cache <NSString, UIImage> = Cache()
     static let imageCache: NSCache<NSString, UIImage> = NSCache()
@@ -84,6 +87,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
             addImage.image = image
+            imageSelected = true
         } else {
             print("RESULT: Image not selected")
         }
@@ -95,6 +99,55 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
         present(imagePicker, animated: true, completion: nil)
     }
     
+    @IBAction func postTapped(_ sender: Any) {
+        guard let caption = captionField.text, caption != "" else {
+            print("RESULT: caption must be entered")
+            return
+        }
+        
+        guard let img = addImage.image, imageSelected == true else {
+            print("RESULT: an image must be selected")
+            return
+        }
+        
+        if let imgData = UIImageJPEGRepresentation(img, 0.2) {
+            let imgUid = NSUUID().uuidString
+            let metadata = FIRStorageMetadata()
+            metadata.contentType = "image/jpeg"
+            
+            DataService.ds.Ref_Posts_Images.child(imgUid).put(imgData, metadata: metadata) { (metadata, error) in
+                if error != nil {
+                    print("RESULT: Unable to upload image to Firebase")
+                } else {
+                    print("RESULT: Succesfully uploaded image to Firebase")
+                    let downloadUrl = metadata?.downloadURL()?.absoluteString
+                    if let url = downloadUrl {
+                        self.uploadToFirebase(imgUrl: url)
+                    }
+                    
+                }
+            }
+        }
+    }
+    
+    func uploadToFirebase(imgUrl: String) {
+        let post: Dictionary<String, AnyObject> = [
+        "caption": captionField.text as AnyObject,
+        "imageUrl": imgUrl as AnyObject,
+        "likes": 0 as AnyObject
+        ]
+        let firebasePost = DataService.ds.Ref_Posts.childByAutoId()
+        firebasePost.setValue(post)
+        
+        mainTable.reloadData()
+        
+        captionField.text = ""
+        imageSelected = false
+        addImage.image = #imageLiteral(resourceName: "add-image")
+        
+        
+        
+    }
     
     @IBAction func signOutTapped(_ sender: Any) {
         
