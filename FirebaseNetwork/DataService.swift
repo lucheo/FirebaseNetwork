aee9988721f0c0be7987f8618a27884960de85c2
Lucheo Antonio Tombini Filho <lucheo@Lucheos-MacBook-Pro.local>
2017-01-11 11:11:46 -0200

    Firebase storage completed

--- FirebaseNetwork/DataService.swift
+++ FirebaseNetwork/DataService.swift
@@ -9,14 +9,18 @@
 import Foundation
 import Firebase
 let DB_BASE = FIRDatabase.database().reference()
+let STORAGE_BASE = FIRStorage.storage().reference()
 
 class DataService {
     
     static let ds = DataService()
-    
+    //DB references
     private var _Ref_Base = DB_BASE
     private var _Ref_Posts = DB_BASE.child("posts")
     private var _Ref_Users = DB_BASE.child("users")
+    
+    //Storage references
+    private var _Ref_Posts_Images = STORAGE_BASE.child("postPics")
     
     var Ref_Base: FIRDatabaseReference {
         return _Ref_Base
@@ -29,6 +33,10 @@ class DataService {
     var Ref_Users: FIRDatabaseReference {
         return _Ref_Users
         
+    }
+    
+    var Ref_Posts_Images: FIRStorageReference {
+        return _Ref_Posts_Images
     }
     
     func createFirebaseDBuser(uid: String, userData: Dictionary<String, String>) {
--- FirebaseNetwork/PostCell.swift
+++ FirebaseNetwork/PostCell.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 class PostCell: UITableViewCell {
     
     @IBOutlet weak var profileImage: UIImageView!
@@ -23,13 +23,32 @@ class PostCell: UITableViewCell {
         // Initialization code
     }
     
-    func configureCell(post: Post) {
+    func configureCell(post: Post, img: UIImage?) {
         self.post = post
         self.caption.text = post.caption
         self.likesLbl.text = "\(post.likes)"
         
+        if img != nil {
+            self.postImg.image = img
         
-    }
+        } else {
+            let ref = FIRStorage.storage().reference(forURL: post.imageUrl)
+            ref.data(withMaxSize: 2 * 1024 * 1024, completion: { (data, error) in
+                if error != nil {
+                    print("RESULT: Unable to download image from Firebase")
+                } else {
+                    print("RESULT: Succesfully downloaded image from Firebase")
+                    if let imgData = data {
+                        if let img = UIImage(data: imgData) {
+                            self.postImg.image = img
+                            FeedVC.imageCache.setObject(img, forKey: post.imageUrl as NSString)
+                        }
+                    }
+                }
+            })
+        }
+        
+        }
 
 
 }
